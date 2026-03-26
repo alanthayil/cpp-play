085116d11542ce0f7dc44bdfc2e4cdd90ed568c0
Alan Michael Thayil <alan-michael.thayil@NEURA-3913.nad.neura-robotics.com>
2026-03-26 20:37:16 +0100

    Day 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Day 1: C++ Robotics Journey Started 🚀" << std::endl;
+struct Point {
+    double x, y, z;
+};
+
+int main(){
+    Point p{1.0, 2.0, 3.0};
+
+    std::cout << "Point : ("
+              << p.x << ", "
+              << p.y << ", "
+              << p.z << ")" << std:: endl;
+
     return 0;
 }
